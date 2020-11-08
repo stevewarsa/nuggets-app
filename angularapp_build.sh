396155c2670a89e77a6dacf5f15d7c5dd715a71e
Steve Warsa <swarsa480@gmail.com>
2020-11-08 06:23:33 -0700

    Update the build script to insert build date/time

--- angularapp_build.sh
+++ angularapp_build.sh
@@ -1,7 +1,9 @@
 cd /cygdrive/c/backup/code/nuggets-app/memory
-currDate=`date +"%m-%d-%Y"`
-sed -i 's/{{todaystr}}/$currDate/g' src/environments/environment.prod.ts
+currDate=$(date +"%m-%d-%Y hh:mm:ss")
+sed -i "s/{{todaystr}}/${currDate}/g" src/environments/environment.prod.ts
 ng build --prod --base-href /nuggets/
+# now put it back to the way it was...
+sed -i "s/${currDate}/{{todaystr}}/g" src/environments/environment.prod.ts
 # delete the existing files for this app
 ssh swarsa_memoryverses@ssh.phx.nearlyfreespeech.net "cd /home/public/nuggets; ls | grep -v server | grep -v browsequotes | grep -v bibleSearch | grep -v chapterSelection | grep -v myverselist | grep -v practice | grep -v practiceSetup | grep -v viewChapter | grep -v viewPassage | grep -v browseTopic | grep -v '^main$' | xargs rm -rf"
 cd dist/memory
